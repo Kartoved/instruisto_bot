9bb926001397d9413807babc1151cd3a1f407ad6
KarToveD <67981770+Kartoved24@users.noreply.github.com>
2023-04-15 16:49:41 +0300

    commit

---
+++ lexicon/lexicon.py
@@ -0,0 +1,15 @@
+HELP_COMMAND = '''Привет, эсперантист!
+Я бот-преподаватель языка <strong>Эсперанто</strong>.
+Я помогу тебе выучить этот прекрасный язык, расскажу много интересного
+и полезного об Эсперанто. Ознакомься с моими командами ниже:
+
+<strong>/help</strong> - <em>список команд</em>
+<strong>/start</strong> - <em>запустить бота</em>
+<strong>/contact</strong> - <em>связаться с разработчиком</em>
+<strong>/links</strong> - <em>полезные ссылки</em>
+<strong>/learning</strong> - <em>учить новые слова</em>
+<strong>/training</strong> - <em>запоминать изученные слова</em>
+'''
+
+
+LEXICON_RU = ''
